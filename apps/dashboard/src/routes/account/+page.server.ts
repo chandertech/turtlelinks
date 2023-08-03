fa902b3b68ba0fee852adfc8ba333e5fb23a6945
Jason Rai <me@jasonrai.ca>
2023-08-03 19:48:18 -0400

    feat: copy newest template

--- apps/dashboard/src/routes/account/+page.server.ts
+++ apps/dashboard/src/routes/account/+page.server.ts
@@ -1,6 +1,7 @@
 import { fail, redirect } from '@sveltejs/kit';
+import type { PageServerLoad } from './$types';
 
-export const load = async ({ locals: { supabase, getSession } }) => {
+export const load: PageServerLoad = async ({ locals: { supabase, getSession } }) => {
 	const session = await getSession();
 
 	if (!session) {
@@ -9,7 +10,7 @@ export const load = async ({ locals: { supabase, getSession } }) => {
 
 	const { data: profile } = await supabase
 		.from('profiles')
-		.select(`username, full_name, website, avatar_url`)
+		.select(`username, full_name, website`)
 		.eq('id', session.user.id)
 		.single();
 
@@ -19,10 +20,9 @@ export const load = async ({ locals: { supabase, getSession } }) => {
 export const actions = {
 	update: async ({ request, locals: { supabase, getSession } }) => {
 		const formData = await request.formData();
-		const fullName = formData.get('fullName') as string;
-		const username = formData.get('username') as string;
-		const website = formData.get('website') as string;
-		const avatarUrl = formData.get('avatarUrl') as string;
+		const fullName = formData.get('fullName') as string | null;
+		const username = formData.get('username') as string | null;
+		const website = formData.get('website') as string | null;
 
 		const session = await getSession();
 
@@ -33,27 +33,25 @@ export const actions = {
 
 		const { error } = await supabase.from('profiles').upsert({
 			id: session?.user.id,
-			full_name: fullName,
-			username,
-			website,
-			avatar_url: avatarUrl,
+			full_name: fullName ? fullName : null,
+			username: username ? username : null,
+			website: website ? website : null,
 			updated_at: new Date().toISOString()
 		});
 
 		if (error) {
+			console.error(error);
 			return fail(500, {
 				fullName,
 				username,
-				website,
-				avatarUrl
+				website
 			});
 		}
 
 		return {
 			fullName,
 			username,
-			website,
-			avatarUrl
+			website
 		};
 	},
 	signout: async ({ locals: { supabase, getSession } }) => {
