b5eb707788815bb6768a8b15c96252721afee7ab
Matthew Zegar <matthewzegar@gmail.com>
2023-08-07 10:41:02 -0700

    WIP create link call

--- apps/dashboard/src/routes/links/+page.server.ts
+++ apps/dashboard/src/routes/links/+page.server.ts
@@ -1,4 +1,4 @@
-import { redirect } from '@sveltejs/kit';
+import { fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
@@ -7,4 +7,39 @@ export const load: PageServerLoad = async ({ url, locals: { getSession } }) => {
 	if (!session) {
 		throw redirect(303, '/login');
 	}
+};
+
+export const actions = {
+	create: async ({ request, locals: { supabase, getSession } }) => {
+		const formData = await request.formData();
+
+		const linkname = formData.get('link-name') as string | null;
+		const subdomain = formData.get('subdomain') as string | null;
+
+		const session = await getSession();
+
+		if (!session) {
+			// log and display error message
+			return;
+		}
+
+		const { error } = await supabase.from('links').upsert({
+			id: session?.user.id,
+			link_name: linkname,
+			subdomain: subdomain
+		});
+
+		if (error) {
+			console.error(error);
+			return fail(500, {
+				linkname,
+				subdomain
+			});
+		}
+
+		return {
+			linkname,
+			subdomain
+		};
+	},
 };
--- apps/dashboard/src/routes/links/+page.svelte
+++ apps/dashboard/src/routes/links/+page.svelte
@@ -12,7 +12,8 @@
 		type: 'component',
 		component: {
 			ref: CreateLinkModal
-		}
+		},
+		response: (r: any) => {}
 	};
 </script>
 
@@ -22,10 +23,26 @@
 
 <div class="mx-auto container p-4">
 	<h1 class="mb-4 text-2xl">Links</h1>
-	<button
+	<!-- TODO: Figure out how to validte and call server from modal. -->
+	<!-- <button
 		class="btn variant-ringed-secondary ml-1"
 		on:click={() => {
 			modalStore.trigger(modal);
 		}}><Fa icon={faAdd} /><span>Create New Link</span></button
-	>
+	> -->
+	<!-- Remove and replace with modal above. -->
+	<form method="POST" action="?/create">
+		<label>
+			Link Name
+			<input name="link-name" class="input" type="text" placeholder="My awesome link" required />
+		</label>
+		<label>
+			Subdomain
+			<div class="input-group input-group-divider grid-cols-2">
+				<input type="search" name="subdomain" placeholder="subdomain" required />
+				<div class="input-group-shim">[todo.com]</div>
+			</div>
+		</label>
+		<button class="btn variant-ringed-secondary">Create Link</button>
+	</form>
 </div>
