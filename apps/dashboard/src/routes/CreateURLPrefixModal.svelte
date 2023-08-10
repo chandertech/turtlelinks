fc1052014a63e9c0539b67c73271855f8ee09db2
Matthew Zegar <matthewzegar@gmail.com>
2023-08-10 09:33:30 -0700

    Improve error checking in create prefix modal

--- apps/dashboard/src/routes/CreateURLPrefixModal.svelte
+++ apps/dashboard/src/routes/CreateURLPrefixModal.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	export let parent: any;
 
-	import { faCheck } from '@fortawesome/free-solid-svg-icons';
+	import { faCheck, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 
 	import { modalStore } from '@skeletonlabs/skeleton';
 	import { Stepper, Step } from '@skeletonlabs/skeleton';
 
-	let domain = '';
-	$: isDomainValid = domain.length != 0; // TODO: Validate more.
+	let subdomain = '';
+	let domain = '.turt.link';
+	$: isValid = subdomain.length > 0 && subdomain.endsWith(domain);
+	$: showWarning = subdomain.length > 0 && !isValid;
 
 	function onFormSubmit(event: Event): void {
-		if ($modalStore[0].response) $modalStore[0].response(domain);
+		if ($modalStore[0].response) $modalStore[0].response(subdomain);
 		modalStore.close();
 	}
 </script>
@@ -19,11 +21,16 @@
 {#if $modalStore[0]}
 	<div class="card p-8 w-modal shadow-xl space-y-4">
 		<Stepper on:complete={onFormSubmit}>
-			<Step locked={!isDomainValid}>
+			<Step locked={!isValid}>
 				<svelte:fragment slot="header">Add URL prefix</svelte:fragment>
 				<label class="label">
 					<span>Domain</span>
-					<input bind:value={domain} class="input" title="domain" type="text" placeholder="" />
+					<input bind:value={subdomain} class="input" title="domain" type="text" placeholder="" />
+					{#if showWarning}
+						<span class="flex text-xs text-red-500"
+							><Fa class="place-self-center pr-1" icon={faExclamationCircle} />Domain must end with {domain}</span
+						>
+					{/if}
 				</label>
 			</Step>
 			<Step>
@@ -31,7 +38,7 @@
 				<div class="card flex flex-row variant-filled-success p-2 px-4">
 					<span class="flex"
 						><Fa class="place-self-center pr-3" icon={faCheck} />
-						{domain} has been verified and approved for use</span
+						{subdomain + domain} has been verified and approved for use</span
 					>
 				</div>
 			</Step>
