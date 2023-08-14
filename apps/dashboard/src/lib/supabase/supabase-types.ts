96c713deaee4cb88bb493f0e68902318140c7e40
Jason Rai <me@jasonrai.ca>
2023-08-14 18:21:13 -0400

    chore: fix supabase client to have types instead of any on dashboard

--- apps/dashboard/src/lib/supabase/supabase-types.ts
+++ apps/dashboard/src/lib/supabase/supabase-types.ts
@@ -1,4 +1,4 @@
-import type { Database } from "../../types/database.types";
+import type { Database } from '../../types/database.types';
 
 export type URLInfo = Database['public']['Tables']['urls']['Row'];
 export type LinkInfo = Database['public']['Tables']['dynamic_links']['Row'];
