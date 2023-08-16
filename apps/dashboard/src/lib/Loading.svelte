cc7d6313df91308649cb7b7f547043f62ad55a76
Matthew Zegar <matthewzegar@gmail.com>
2023-08-16 13:11:47 -0700

    Leaving the warning for now

--- apps/dashboard/src/lib/Loading.svelte
+++ apps/dashboard/src/lib/Loading.svelte
@@ -6,7 +6,6 @@
 
 <section class="card w-full">
 	<div class="p-4 space-y-{space}">
-		<!-- svelte-ignore @typescript-eslint/no-unused-vars -->
 		{#each { length: rows } as _, i}
 			{#if i % 2 === 0}
 				<div class="grid grid-cols-3 gap-8">
