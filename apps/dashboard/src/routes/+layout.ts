b7e19950fc276dc2d22269872387dd6d4e4ed1ab
Matthew Zegar <matthewzegar@gmail.com>
2023-08-17 08:23:48 -0700

    Create superbase admin client, move database types to lib

--- apps/dashboard/src/routes/+layout.ts
+++ apps/dashboard/src/routes/+layout.ts
@@ -1,7 +1,7 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 import type { LayoutLoad } from './$types';
-import type { Database } from '../types/database.types';
+import type { Database } from '$lib/types/database.types';
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
