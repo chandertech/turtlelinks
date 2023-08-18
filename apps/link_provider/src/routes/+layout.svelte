52cd52e39ae66024a08c75646d90548cfb4261a9
Matthew Zegar <matthewzegar@gmail.com>
2023-08-18 08:50:00 -0700

    Init link_provider app

---
+++ apps/link_provider/src/routes/+layout.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '../theme.postcss';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppShell, AppBar, Avatar } from '@skeletonlabs/skeleton';
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	import { faLink } from '@fortawesome/free-solid-svg-icons';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+	import Fa from 'svelte-fa';
+
+	export let data;
+
+	let { supabase, session } = data;
+	$: ({ supabase, session } = data);
+
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((event, _session) => {
+			if (_session?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+
+		return () => data.subscription.unsubscribe();
+	});
+</script>
+
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead"
+				><a href="/" class="text-xl font-bold"
+					><button type="button" class="btn variant-filled-surface">
+						<Fa icon={faLink} />
+						<span>Link Provider</span>
+					</button></a
+				></svelte:fragment
+			>
+
+			<svelte:fragment slot="trail">
+				<a href="https://github.com/chandertech/turtlelinks"
+					><button type="button" class="btn-icon variant-filled-surface">
+						<Fa icon={faGithub} />
+					</button></a
+				>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<slot />
+</AppShell>
---
+++ apps/link_provider/src/routes/account/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import type { SubmitFunction } from '@sveltejs/kit';
+
+	export let data;
+	export let form;
+
+	let { session, profile } = data;
+
+	let profileForm: HTMLFormElement;
+	let loading = false;
+	let fullName = profile?.full_name ?? null;
+	let username = profile?.username ?? null;
+	let website = profile?.website ?? null;
+
+	const handleSubmit: SubmitFunction = () => {
+		loading = true;
+		return async () => {
+			loading = false;
+		};
+	};
+
+	const handleSignOut: SubmitFunction = () => {
+		loading = true;
+		return async ({ update }) => {
+			loading = false;
+			update();
+		};
+	};
+</script>
+
+<div class="mx-auto container p-4">
+	<h1 class="mb-4">Account Information</h1>
+	<form
+		id="account-info"
+		method="post"
+		action="?/update"
+		use:enhance={handleSubmit}
+		bind:this={profileForm}
+	/>
+	<form id="sign-out" method="post" action="?/signout" use:enhance={handleSignOut} />
+
+	<label for="email" class="label">Email</label>
+	<input
+		id="email"
+		form="account-info"
+		type="text"
+		class="input mb-4"
+		value={session.user.email}
+		disabled
+	/>
+	<label for="fullName" class="label">Full Name</label>
+	<input
+		id="fullName"
+		form="account-info"
+		name="fullName"
+		type="text"
+		class="input mb-4"
+		value={form?.fullName ?? fullName}
+	/>
+	<label for="username" class="label">Username</label>
+	<input
+		id="username"
+		form="account-info"
+		name="username"
+		type="text"
+		class="input mb-4"
+		value={form?.username ?? username}
+	/>
+	<label for="website" class="label">Website</label>
+	<input
+		id="website"
+		form="account-info"
+		name="website"
+		type="url"
+		class="input mb-4"
+		value={form?.website ?? website}
+	/>
+	<div>
+		<input
+			type="submit"
+			form="account-info"
+			class="btn variant-filled-primary"
+			value={loading ? 'Loading...' : 'Update'}
+			disabled={loading}
+		/>
+
+		<button class="btn variant-filled-secondary ml-4" form="sign-out" disabled={loading}
+			>Sign Out</button
+		>
+	</div>
+</div>
