de3a55354d58bd93b2ec42e7d6a18e811314eb69
Matthew Zegar <matthewzegar@gmail.com>
2023-08-31 11:38:05 -0700

    Setup example subscription, try out redirecting to stripe page

--- apps/dashboard/src/routes/api/stripe-checkout/+server.ts
+++ apps/dashboard/src/routes/api/stripe-checkout/+server.ts
@@ -1,6 +1,19 @@
 import { error, json } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
+import { stripeAdminClient } from '$lib/stripe/stripe';
 
-export const POST: RequestHandler = async () => {
-	return json({ success: true });
+export const POST: RequestHandler = async ({ request }) => {
+	const session = await stripeAdminClient.checkout.sessions.create({
+		mode: 'subscription',
+		success_url: `https://buy.stripe.com/test_4gw17IaTv73f1l67ss`,
+		cancel_url: `/error`,
+		line_items: [
+			{
+				quantity: 1,
+				price: 'price_1NlFXaDYh43AXwRboHsILqAp'
+			}
+		]
+	});
+
+	return json({ url: session.url });
 };
--- apps/dashboard/src/routes/payment/+page.svelte
+++ apps/dashboard/src/routes/payment/+page.svelte
@@ -1,4 +1,26 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
+	async function subscribe() {
+		const res = await fetch('/api/stripe-checkout', {
+			method: 'POST'
+		});
+
+		if (res.ok) {
+			const { url } = await res.json();
+			goto(url);
+		}
+	}
 </script>
 
-<div class="flex flex-col gap-4 items-center py-24"></div>
+<div class="flex flex-col gap-4 items-center py-24">
+	<button
+		type="button"
+		class="btn variant-ghost-primary"
+		on:click={() => {
+			subscribe();
+		}}
+	>
+		<span>Stripe Test</span>
+	</button>
+</div>
