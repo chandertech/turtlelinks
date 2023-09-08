b64d11429c789f539002e4c719bf78155b204adf
Matthew Zegar <matthewzegar@gmail.com>
2023-09-08 14:06:37 -0700

    Make sure we sync the database with products/prices

--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -1,13 +1,32 @@
-import { createOrRetrieveCustomer } from '$lib/stripe/stripe-billing-helpers';
-import { error } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
+import { building } from '$app/environment';
+import { stripeAdminClient } from '$lib/stripe/stripe-admin-client';
+import { upsertPriceRecord, upsertProductRecord } from '$lib/stripe/stripe-billing-helpers';
+
+if (!building) {
+	stripeAdminClient.products
+		.list()
+		.then(async (products) => {
+			for (const product of products.data) {
+				await upsertProductRecord(product);
+			}
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+
+	stripeAdminClient.prices
+		.list()
+		.then(async (prices) => {
+			for (const price of prices.data) {
+				await upsertPriceRecord(price);
+			}
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+}
 
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
-	const session = await getSession();
-	if (!session) throw error(400);
-
-	// TODO: Potentially move somewhere else? Maybe inside the webhook? Also email is potentially null...
-	await createOrRetrieveCustomer({ profileId: session.user.id, email: session.user.email || '' });
-
-	return { session: session };
+	return { session: await getSession() };
 };
