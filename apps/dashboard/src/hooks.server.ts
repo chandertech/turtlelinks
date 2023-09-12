eb88f0cc523ad80574bb44cf883932eead29f378
Matthew Zegar <matthewzegar@gmail.com>
2023-09-12 08:37:12 -0700

    Move it back to layout.server for now

--- apps/dashboard/src/hooks.server.ts
+++ apps/dashboard/src/hooks.server.ts
@@ -2,8 +2,6 @@
 import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
 import { createSupabaseServerClient } from '@supabase/auth-helpers-sveltekit';
 import type { Handle } from '@sveltejs/kit';
-import { stripeAdminClient } from '$lib/stripe/stripe-admin-client';
-import { upsertPriceRecord, upsertProductRecord } from '$lib/stripe/stripe-billing-helpers';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.supabase = createSupabaseServerClient({
@@ -22,37 +20,9 @@ export const handle: Handle = async ({ event, resolve }) => {
 		return session;
 	};
 
-	await syncStripe();
-
 	return resolve(event, {
 		filterSerializedResponseHeaders(name) {
 			return name === 'content-range';
 		}
 	});
 };
-
-async function syncStripe() {
-	stripeAdminClient.products
-		.list()
-		.then(async (products) => {
-			const promises = products.data.map(async (product) => {
-				await upsertProductRecord(product);
-			});
-			await Promise.all(promises);
-		})
-		.catch((e) => {
-			console.log(e);
-		});
-
-	stripeAdminClient.prices
-		.list()
-		.then(async (prices) => {
-			const promises = prices.data.map(async (price) => {
-				await upsertPriceRecord(price);
-			});
-			await Promise.all(promises);
-		})
-		.catch((e) => {
-			console.log(e);
-		});
-}
--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -1,4 +1,34 @@
 import type { LayoutServerLoad } from './$types';
+import { building } from '$app/environment';
+import { stripeAdminClient } from '$lib/stripe/stripe-admin-client';
+import { upsertPriceRecord, upsertProductRecord } from '$lib/stripe/stripe-billing-helpers';
+
+// TODO: Might need to move this somewhere else before merge...
+if (!building) {
+	stripeAdminClient.products
+		.list()
+		.then(async (products) => {
+			const promises = products.data.map(async (product) => {
+				await upsertProductRecord(product);
+			});
+			await Promise.all(promises);
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+
+	stripeAdminClient.prices
+		.list()
+		.then(async (prices) => {
+			const promises = prices.data.map(async (price) => {
+				await upsertPriceRecord(price);
+			});
+			await Promise.all(promises);
+		})
+		.catch((e) => {
+			console.log(e);
+		});
+}
 
 export const load: LayoutServerLoad = async ({ locals: { getSession } }) => {
 	return { session: await getSession() };
